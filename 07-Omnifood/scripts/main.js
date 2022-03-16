263c8f3487188f63c2c349a1d64358f4cb8716b2
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2022-03-16 13:04:36 -0300

    finish the mobile navigation

--- 07-Omnifood/scripts/main.js
+++ 07-Omnifood/scripts/main.js
@@ -1,8 +1,10 @@
+// Set current year to copyright
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-const mobileNav = document.querySelector(".btn-mobile-nav");
-mobileNav.addEventListener("click", function () {
-  document.querySelector(".header").classList.toggle("nav-open");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
